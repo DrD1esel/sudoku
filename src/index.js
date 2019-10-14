d9dce22fa4ef05129a7b35140c28c0b54204a76d
DrD1esel <kolonicki.e@gmail.com>
2019-10-14 12:39:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  var sudoku=matrix.map(x=>{return x.join("")}).join(",")  
+  return next(sudoku).split(",").map(x=>{ return x.split("").map(z=>{return +z})})
 }
+
+function getPossibleDigits(row,column,str){
+  var array=str.split(",")
+  var hor=array[row]
+  var ver=array.map(x=>{return x.charAt(column)})
+  var sqrRow=Math.floor(row/3)*3
+  var sqrColumn=Math.floor(column/3)*3    
+  var sqr=[0,1,2].map(x=>{return array[sqrRow+x].substr(sqrColumn,3)}).join("")    
+  return "123456789".replace(new RegExp("["+hor+ver+sqr+"]","g") ,"").split("")
+}
+
+function next(str){    
+  var index = str.replace(/,/g,"").indexOf("0")
+  if (index==-1) return str    
+  var digits = getPossibleDigits(Math.floor(index/9),index%9,str)
+  if (!digits[0]) return false 
+  for (var i=0;i<digits.length; i++){      
+    var temp = str.replace("0",digits[i])     
+    var ch = next(temp)
+    if(ch) return ch
+  }    
+}g
